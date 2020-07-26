a88c34f6c337b0bad2226ceb66b5036e793e3d46
AlFed18 <fedor_001@mail.ru>
2020-07-26 10:41:30 +0300

    добавил скрипт просмотра забронированных билетов

---
+++ 04_LookItinerary/data/action.c
@@ -0,0 +1,108 @@
+Action()
+{
+
+	web_set_sockets_option("SSL_VERSION", "2&3");
+
+	web_add_auto_header("Sec-Fetch-Site", 
+		"none");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"document");
+
+	web_add_auto_header("Sec-Fetch-Mode", 
+		"navigate");
+
+	web_add_auto_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_url("WebTours", 
+		"URL=http://localhost:1080/WebTours/", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_start_transaction("Login");
+
+	web_revert_auto_header("Sec-Fetch-User");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"frame");
+
+	web_revert_auto_header("Upgrade-Insecure-Requests");
+
+	web_add_header("Origin", 
+		"http://localhost:1080");
+
+	web_add_auto_header("Sec-Fetch-Site", 
+		"same-origin");
+
+	lr_think_time(20);
+
+	web_submit_data("login.pl", 
+		"Action=http://localhost:1080/cgi-bin/login.pl", 
+		"Method=POST", 
+		"TargetFrame=body", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?in=home", 
+		"Snapshot=t2.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=userSession", "Value=129255.629566626zztifizpttfiDDDDDQiQVpfVizHf", ENDITEM, 
+		"Name=username", "Value=gogi", ENDITEM, 
+		"Name=password", "Value=qwerty", ENDITEM, 
+		"Name=login.x", "Value=51", ENDITEM, 
+		"Name=login.y", "Value=6", ENDITEM, 
+		"Name=JSFormSubmit", "Value=off", ENDITEM, 
+		LAST);
+
+	lr_end_transaction("Login",LR_AUTO);
+
+	lr_start_transaction("Click_Itinerary");
+
+	web_add_auto_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	lr_think_time(26);
+
+	web_url("Itinerary Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?page=itinerary", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("Click_Itinerary",LR_AUTO);
+
+	lr_start_transaction("Click_Home");
+
+	web_revert_auto_header("Sec-Fetch-User");
+
+	lr_think_time(12);
+
+	web_url("Home Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?page=menus", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=itinerary", 
+		"Snapshot=t4.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("Click_Home",LR_AUTO);
+
+	return 0;
+}
